88aaf012ae46fb6ffc2bece27c04dbafc5776192
jlomako <71858760+jlomako@users.noreply.github.com>
2022-12-13 10:56:56 -0500

    Update message.py
    test with other email

--- message.py
+++ message.py
@@ -18,13 +18,13 @@ message = response["choices"][0]["text"]
 
 # send message to myself
 my_email = os.environ['MY_EMAIL']
-# my_email2 = os.environ['MY_EMAIL2']
+my_email2 = os.environ['MY_EMAIL2']
 pw = os.environ['PASSWORD']
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as connection:
     connection.starttls()
     connection.login(user=my_email, password=pw)
     connection.sendmail(from_addr=my_email,
-                        to_addrs=my_email,
+                        to_addrs=my_email2,
                         msg=f'Subject: hello\n\n{message}'
                         )
