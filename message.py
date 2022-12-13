8b8c633719e34f028dd710cccb2234c6eca80b0d
jlomako <71858760+jlomako@users.noreply.github.com>
2022-12-13 10:50:46 -0500

    Update message.py
    removed variable

--- message.py
+++ message.py
@@ -1,4 +1,3 @@
-
 import smtplib
 import os
 import openai
@@ -19,11 +18,11 @@ message = response["choices"][0]["text"]
 
 # send message to myself
 my_email = os.environ['MY_EMAIL']
-your_email = os.environ['MY_EMAIL2']
+# my_email2 = os.environ['MY_EMAIL2']
 pw = os.environ['PASSWORD']
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as connection:
-    connection.starttls() # adds encryption
+    connection.starttls()
     connection.login(user=my_email, password=pw)
     connection.sendmail(from_addr=my_email,
                         to_addrs=my_email,
